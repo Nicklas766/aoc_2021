977379443f9a16e9cb8589890338bcda599546b3
nicklas766 <nicklas766@live.se>
2021-11-24 09:15:22 +0100

    add 0

--- day01/src/solutions.js
+++ day01/src/solutions.js
@@ -1,7 +1,7 @@
 const { isPrime, isEven } = require('./utils');
 
 const getSolutionPart1 = (numbers) => numbers.reduce(
-  (acc, num, i) => (isPrime(num) ? acc + (num * i) : acc),
+  (acc, num, i) => (isPrime(num) ? acc + (num * i) : acc), 0
 );
 
 const getSolutionPart2 = (numbers) => numbers.reduce((acc, num, i) => {
@@ -10,7 +10,7 @@ const getSolutionPart2 = (numbers) => numbers.reduce((acc, num, i) => {
   }
 
   return isEven(i) ? acc + num : acc - num;
-});
+}, 0);
 
 module.exports = {
   getSolutionPart1,
