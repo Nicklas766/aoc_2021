67901ebf7deafa980ddeb9dbf23632f48ec1c444
Nicklas Envall <nicklasenvallcygni@outlook.com>
2021-11-30 18:56:13 +0100

    add base

---
+++ base/src/solutions.js
@@ -0,0 +1,12 @@
+const getSolutionPart1 = (input) => {
+  return input;
+}
+
+const getSolutionPart2 = (input) => {
+  return input;
+}
+
+module.exports = {
+  getSolutionPart1,
+  getSolutionPart2,
+};
