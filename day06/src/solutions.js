050f1fb789ed047115f2993506fc4580802e339e
nicklas766 <nicklas766@live.se>
2021-12-06 19:18:47 +0100

    day06 - might refactor later

---
+++ day06/src/solutions.js
@@ -0,0 +1,23 @@
+const getLanternfishSpawnAmountAfterDays = (n, inputArray) => {
+  const spawnDaysToAmount = { ...Array.from({ length: 8 + 1 }, () => 0) };
+  inputArray.forEach((numOfDays) => spawnDaysToAmount[numOfDays] += 1);
+
+  for (let i = 0; i < n; i++) {
+    const readyToSpawnAmount = spawnDaysToAmount[0];
+  
+    for (let t = 0; t <= 8; t++) {
+      spawnDaysToAmount[t] = spawnDaysToAmount[t + 1];
+    }
+  
+    spawnDaysToAmount[8] = 0;
+    spawnDaysToAmount[6] += readyToSpawnAmount;
+    spawnDaysToAmount[8] += readyToSpawnAmount;
+  }
+
+  return Object.values(spawnDaysToAmount).reduce((acc, curr) => acc + curr);
+}
+
+module.exports = {
+  getSolutionPart1: (input) => getLanternfishSpawnAmountAfterDays(80, input),
+  getSolutionPart2: (input) => getLanternfishSpawnAmountAfterDays(256, input),
+};
